f9a0f613de40ad5a506a7a13055d238e1c753507
Raysk <raysk7161@gmail.com>
2019-08-28 13:50:22 +0530

    added put for categories

--- routes/categories.js
+++ routes/categories.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
 const { validate, Category } = require("../models/categories");
 
 router.get("/", async (req, res) => {
   res.json({ results: await Category.find() });
 });
-
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error)
@@ -22,6 +23,21 @@ router.post("/", async (req, res) => {
   });
   const result = await category.save();
   res.json(result);
+});
+router.put("/:id", async (req, res) => {
+  const isValid = mongoose.Types.ObjectId.isValid(req.params.id);
+  if (!isValid) return res.status(400).send(`invalid category id..`);
+
+  const categoryExitits = await Category.findOne({ title: req.body.title });
+  if (categoryExitits)
+    return res.status(400).send(`category already exitis in the database`);
+
+  let category = await Category.findByIdAndUpdate(req.params.id, {
+    title: req.body.title
+  });
+  if (!category) return res.status(400).send(`no record found.`);
+
+  res.send(`updated successfully`);
 });
 
 module.exports = router;
